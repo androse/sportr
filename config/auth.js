59bb1955438167b4394c969a7e306bc16f7223d5
Andrew Larose <werdnalarose@gmail.com>
2014-01-31 15:29:31 -0500

    facebook keys

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 module.exports = {
 	
 	'facebookAuth': {
-		'clientID': 'our-client-id-goes-here',
-		'clientSecret': 'our-client-secret-goes-here',
+		'clientID': '742183452460714',
+		'clientSecret': '345e434b61aa9bd196b55b3092fc955d',
 		// Route will be the same but http://localhost:8080 will need to be 
 		// changed if hosted on a server
 		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
