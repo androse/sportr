5033524926b5a4ed9cf264f9156c4acb87cce6ca
jstirling91 <jstirling91@gmail.com>
2014-02-01 18:58:15 -0500

    attempt to fix callback redirect

--- config/auth.js
+++ config/auth.js
@@ -4,6 +4,6 @@ module.exports = {
 		'clientSecret': '345e434b61aa9bd196b55b3092fc955d',
 		// Route will be the same but http://localhost:8080 will need to be 
 		// changed if hosted on a server
-		'callbackURL': './auth/facebook/callback'
+		'callbackURL': 'http://www.sportr.heroku.com/auth/facebook/callback'
 	}
 }
