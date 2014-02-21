873f2b4ecfeda4683b075b74d635f7aa62d8e2d8
ashley simpson <ashley.simpson@mail.mcgill.ca>
2014-02-20 21:15:04 -0500

    added warning functionality when searching for empty input to location and date, and updated formatting

---
+++ public/js/search.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	
+	$('#submitsearchlocation').click(function() {
+		var location = $('#searchlocation input:text').val();
+
+		//if user inputted nothing into the location box, then do not send edit location request and display warning
+		if (location == ""){
+			appendDangerAlert('locationerror', 'Please Enter A Location!');	
+		}
+	});
+    
+    $('#submitsearchdate').click(function() {
+		var location = $('#searchdate input:text').val();
+
+		//if user inputted nothing into the location box, then do not send edit location request and display warning
+		if (location == ""){
+			appendDangerAlert('locationerror', 'Please Enter A Date!');	
+		}
+	});
+	
+	//Usage: alertName = any arbitrary class name. 
+	//Prints 'message' if no alerts of name 'alertName' are already present
+	function appendDangerAlert(alertName, message){
+		var type = document.getElementsByClassName(alertName);
+		if(type.length == 0){
+				$('#alertmessages').append('<div class="' + alertName + ' alert alert-danger close fade in" data-dismiss="alert">' + message + '</div>')
+				setTimeout( function() {
+				$('.'+alertName).alert('close');
+				}, 2000 );
+		}
+	}
+});
