9d266d01e47222af692fd45019cbd3fac541bbb1
Vasily Rassokhin <vavaxman@hotmail.com>
2014-03-09 17:12:45 -0400

    user search checks for any username containing search string and is now case insensitive

--- app/database.js
+++ app/database.js
@@ -264,7 +264,7 @@ function search(sport, location, date, successCB, errorCB) {
 
 // Function to search Users
 function searchUser(username, successCB, errorCB, defaultCB){
-    User.findOne({'userName': username}, '_id', function(err, user){
+    User.findOne({'userName': {$regex: username, $options: 'i'}}, '_id', function(err, user){
         if(err) errorCB(err);
         else if(user) successCB(user._id);
         else defaultCB('');
